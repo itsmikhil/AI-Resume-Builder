856b092327c0f1d0c9f21034af0b8616aa3fba26
itsmikhil <ailani.mikhil@gmail.com>
2026-02-19 17:16:45 +0530

    fea: implemented user signIn

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,21 +3,36 @@ import validator from "validator";
 import jwt from "jsonwebtoken";
 import userModel from "../models/user.js";
 
-let generateToken = async (email, id) => {
-  let token = jwt.sign({ email, id }, process.env.JWT_SECRET);
-  return token
+let generateToken = (email, id) => {
+  let token = jwt.sign({ email, id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
 };
 
 let handleUserRegisteration = async (req, res) => {
   try {
     let { name, email, password } = req.body;
 
-    name = name.trim();
-    email = email.trim();
-    password = password.trim();
+    name = name?.trim();
+    email = email?.trim();
+    password = password?.trim();
 
     if (!name || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Password should contain atleast 8 characters",
+      });
     }
 
     let existingUser = await userModel.findOne({ email });
@@ -29,17 +44,6 @@ let handleUserRegisteration = async (req, res) => {
       });
     }
 
-    if (!validator.isEmail(email)) {
-      return res.status(400).json({ message: "Invalid Email" });
-    }
-
-    if (password.length < 8) {
-      return res.status(400).json({
-        success: false,
-        message: "Password should contain atleast 8 characters",
-      });
-    }
-
     let hashedPassword = await bcrypt.hash(password, 10);
 
     let newUser = { name, email, password: hashedPassword };
@@ -48,12 +52,10 @@ let handleUserRegisteration = async (req, res) => {
 
     result.password = undefined;
 
-    let token = await generateToken(email, result._id);
-
-    console.log(token);
+    let token = generateToken(email, result._id);
 
     return res.status(200).json({
-      message: "user created successfully",
+      message: "Welcome!",
       success: true,
       user: result,
       token,
@@ -66,4 +68,55 @@ let handleUserRegisteration = async (req, res) => {
   }
 };
 
-export default { handleUserRegisteration };
+let handleUserSignIn = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    email = email?.trim();
+    password = password?.trim();
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+
+    let result = await userModel.findOne({ email });
+
+    if (!result) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect email or password" });
+    }
+
+    let correctPassword = await bcrypt.compare(password, result.password);
+
+    if (!correctPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect email or password" });
+    }
+
+    result.password = undefined;
+
+    let token = generateToken(email, result._id);
+
+    return res.status(200).json({
+      message: "Welcome Back!",
+      success: true,
+      user: result,
+      token,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default { handleUserRegisteration, handleUserSignIn };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,5 +4,6 @@ import userController from "../controllers/userController.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", userController.handleUserRegisteration);
+userRouter.post("/signin", userController.handleUserSignIn);
 
 export default userRouter;
